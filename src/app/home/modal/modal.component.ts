06ec045c7373c347eef7cd76649b8c7a0076f61c
Jim Murphy <murphyj1011@gmail.com>
2022-03-05 11:34:53 -0500

    add toast message for when there are no quizzes for a level

--- src/app/home/modal/modal.component.ts
+++ src/app/home/modal/modal.component.ts
@@ -7,7 +7,7 @@ import {
   OnInit,
 } from '@angular/core';
 import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { getAnalytics, logEvent } from 'firebase/analytics';
 import { Observable, Subscription } from 'rxjs';
 import { UtilitiesService } from '../../utilities.service';
@@ -37,7 +37,8 @@ export class ModalComponent implements OnInit, OnDestroy {
     private router: Router,
     private quizService: QuizService,
     public utitlities: UtilitiesService,
-    private modalController: ModalController
+    private modalController: ModalController,
+    private toastController: ToastController
   ) {}
 
   ngOnInit() {
@@ -52,12 +53,28 @@ export class ModalComponent implements OnInit, OnDestroy {
   }
 
   quizSelected(event: any): void {
-    console.log(event);
-
     this.subSink.add(
       this.quizService
         .getQuiz(event.id, this.difficulty)
-        .subscribe((q: any) => {
+        .subscribe(async (q: any) => {
+          if (q.results.length === 0) {
+            const toast = await this.toastController.create({
+              message: `Sorry, looks like there is no ${event.value} quiz at the ${this.difficulty} level!  
+                You can add some questions at
+                <a target="_blank" rel="noopener noreferrer" href="https://opentdb.com/" style="color: 'slate'">Open Trivia DB</a>`,
+              duration: 4000,
+              color: 'secondary',
+              buttons: [
+                {
+                  text: 'Dismiss',
+                  role: 'cancel',
+                },
+              ],
+            });
+            toast.present();
+            return;
+          }
+
           this.router.navigate([`/quiz/${event.value}`], {
             state: { quiz: q.results, triviaType: event.value },
           });
